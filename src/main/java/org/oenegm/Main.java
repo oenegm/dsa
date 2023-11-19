fbe30cbada3b109082b3fb628d1a0908a28ef4b0
oenegm <oenegm@gmail.com>
2023-11-19 20:41:26 +0200

    implement binary search algorithm

--- src/main/java/org/oenegm/Main.java
+++ src/main/java/org/oenegm/Main.java
@@ -7,12 +7,46 @@ public class Main {
         System.out.println("NOT IMPLEMENTED");
     }
 
-    public static boolean linearSearch(int[] arr, int number) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == number) {
+    public static boolean binarySearchLoop(int[] arr, int value) {
+        int low = 0;
+        int high = arr.length;
+
+        do {
+            int middle = low + (high - low) / 2;
+            int current = arr[middle];
+
+            if (current == value) {
                 return true;
+            } else if (current > value) {
+                high = middle;
+            } else {
+                low = middle + 1;
             }
-        }
+
+        } while (low < high);
+
         return false;
+    }
+
+    public static boolean binarySearchRecursion(int[] arr, int value) {
+        return binarySearchRecursion(arr, value, 0, arr.length);
+    }
+
+    private static boolean binarySearchRecursion(int[] arr, int value, int low, int high) {
+
+        if (low > high) {
+            return false;
+        }
+
+        int middle = low + (high - low) / 2;
+        int current = arr[middle];
+
+        if (current == value) {
+            return true;
+        } else if (current > value) {
+            return binarySearchRecursion(arr, value, low, middle - 1);
+        } else {
+            return binarySearchRecursion(arr, value, middle + 1, high);
+        }
     }
 }
--- src/test/java/org/oenegm/MainTest.java
+++ src/test/java/org/oenegm/MainTest.java
@@ -4,26 +4,46 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.oenegm.Main.linearSearch;
+import static org.oenegm.Main.binarySearchLoop;
+import static org.oenegm.Main.binarySearchRecursion;
 
 class MainTest {
 
     @Test
-    void testExists() {
+    void testExistsLoop() {
 
         int[] testArr = new int[]{
-                1, 2, 3, 3, 243, 2131, 3123, 213, 20, 23123, 213543, 645, 7567, 67568, 6535, 2345234
+                1, 2, 3, 3, 20, 243, 2131, 3123, 23123, 213543
         };
 
-        assertTrue(linearSearch(testArr, 20));
+        assertTrue(binarySearchLoop(testArr, 20));
     }
 
     @Test
-    void testDoesNotExist() {
+    void testDoesNotExistLoop() {
         int[] testArr = new int[]{
-                1, 2, 3, 3, 243, 2131, 3123, 213, 20, 23123, 213543, 645, 7567, 67568, 6535, 2345234
+                1, 2, 3, 3, 20, 243, 2131, 3123, 23123, 213543
         };
 
-        assertFalse(linearSearch(testArr, 50));
+        assertFalse(binarySearchLoop(testArr, 50));
+    }
+
+    @Test
+    void testExistsRecursion() {
+
+        int[] testArr = new int[]{
+                1, 2, 3, 3, 20, 243, 2131, 3123, 23123, 213543
+        };
+
+        assertTrue(binarySearchRecursion(testArr, 20));
+    }
+
+    @Test
+    void testDoesNotExistRecursion() {
+        int[] testArr = new int[]{
+                1, 2, 3, 3, 20, 243, 2131, 3123, 23123, 213543
+        };
+
+        assertFalse(binarySearchRecursion(testArr, 50));
     }
 }
