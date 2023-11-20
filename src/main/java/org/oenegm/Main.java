b2a5acb3442a25e6876052a3b7e63dc8d1c64e13
oenegm <oenegm@gmail.com>
2023-11-20 20:59:13 +0200

    implement bubble sort

--- src/main/java/org/oenegm/Main.java
+++ src/main/java/org/oenegm/Main.java
@@ -4,37 +4,19 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String problem = """
-                    Two Crystal Ball problem:
-                    Given two crystal balls that will break if dropped from high enough distance, determine the exact spot in which it will break in the most optimized way.
-                    Solution: To understand the problem, let us assume the crystal ball will break if dropped from a height of 8 meters. So, if we drop from true meter, the ball will not break(false).
-                    If dropped from height of 2 meter, again the ball will not break(false). We keep on doing it. When dropped from 8 meters, the ball will break(true).
-                    If we list all falses and trues in an array,
-                    it will look like below: [false, false, false, false, false, false, false, false ,false, false, false, true, true, true, true]
-                    This solution is in O(sqrt(n))
-                """;
-        System.out.print(problem);
+
     }
 
-    public static int twoCrystalBalls(boolean[] arr) {
-        int jumpAmount = (int) Math.sqrt(arr.length);
+    public static void bubbleSort(int[] arr) {
 
-        int i = jumpAmount;
-
-        for (; i < arr.length; i += jumpAmount) {
-            if (arr[i]) {
-                break;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
-
-        i -= jumpAmount;
-
-        for (int j = 0; j < jumpAmount && i < arr.length; ++j, ++i) {
-            if (arr[i]) {
-                return i;
-            }
-        }
-        return -1;
     }
-
 }
--- src/test/java/org/oenegm/MainTest.java
+++ src/test/java/org/oenegm/MainTest.java
@@ -1,16 +1,21 @@
 package org.oenegm;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.oenegm.Main.twoCrystalBalls;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.oenegm.Main.bubbleSort;
 
 class MainTest {
 
     @Test
-    void testTwoCrystalBalls() {
+    void testSort() {
+        int[] arr = {23, 414, 4325, 123, 534, 1314, 532, 2423, 131, 67, 567, 456, 36745};
 
-        boolean[] arr = {false, false, false, false, false, false, false, false ,false, false, false, true, true, true, true};
+        int[] expected = {23, 67, 123, 131, 414, 456, 532, 534, 567, 1314, 2423, 4325, 36745};
 
-        assertEquals(11, twoCrystalBalls(arr));
+        bubbleSort(arr);
+
+        assertArrayEquals(expected, arr);
     }
+
 }
