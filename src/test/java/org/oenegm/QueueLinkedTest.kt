2f30b5ba80c8b3de905394fd0b18f73b707d74e2
oenegm <oenegm@gmail.com>
2023-11-22 13:17:22 +0200

    update dependencies

--- src/test/java/org/oenegm/QueueLinkedTest.kt
+++ src/test/java/org/oenegm/QueueLinkedTest.kt
@@ -1,7 +1,8 @@
 package org.oenegm
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
 
 internal class QueueLinkedTest {
     @Test
@@ -12,31 +13,31 @@ internal class QueueLinkedTest {
         queue.enqueue(2)
         queue.enqueue(3)
 
-        Assertions.assertEquals(3, queue.length())
-        Assertions.assertEquals(1, queue.deque())
-        Assertions.assertEquals(2, queue.deque())
-        Assertions.assertEquals(1, queue.length())
+        assertEquals(3, queue.length())
+        assertEquals(1, queue.deque())
+        assertEquals(2, queue.deque())
+        assertEquals(1, queue.length())
 
         queue.enqueue(4)
         queue.enqueue(5)
 
-        Assertions.assertEquals(3, queue.peek())
-        Assertions.assertEquals(3, queue.deque())
-        Assertions.assertEquals(4, queue.deque())
-        Assertions.assertEquals(5, queue.deque())
-        Assertions.assertEquals(0, queue.length())
+        assertEquals(3, queue.peek())
+        assertEquals(3, queue.deque())
+        assertEquals(4, queue.deque())
+        assertEquals(5, queue.deque())
+        assertEquals(0, queue.length())
 
-        Assertions.assertNull(queue.peek())
-        Assertions.assertNull(queue.deque())
+        assertNull(queue.peek())
+        assertNull(queue.deque())
     }
 
     @Test
     fun testEmptyQueue() {
         val queue = QueueLinked<String>()
 
-        Assertions.assertEquals(0, queue.length())
+        assertEquals(0, queue.length())
 
-        Assertions.assertNull(queue.peek())
-        Assertions.assertNull(queue.deque())
+        assertNull(queue.peek())
+        assertNull(queue.deque())
     }
 }
