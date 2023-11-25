b10c55237b3572ce82212f3dc7a4643b0a318b02
oenegm <oenegm@gmail.com>
2023-11-25 15:55:17 +0200

    implement quick sort

--- src/main/java/org/oenegm/Main.java
+++ src/main/java/org/oenegm/Main.java
@@ -4,19 +4,54 @@ public class Main {
 
 
     public static void main(String[] args) {
-
+        System.out.println("Not Implemented");
     }
 
-    public static void bubbleSort(int[] arr) {
+    public static void bubbleSort(int[] array) {
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
+    }
+
+    public static void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] array, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int pivotIndex = partition(array, low, high);
+
+        quickSort(array, low, pivotIndex - 1);
+        quickSort(array, pivotIndex + 1, high);
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+
+        int index = low - 1;
+
+        for (int i = low; i < high; i++) {
+            if (array[i] <= pivot) {
+                index++;
+                int temp = array[i];
+                array[i] = array[index];
+                array[index] = temp;
+            }
+        }
+
+        index++;
+        array[high] = array[index];
+        array[index] = pivot;
+
+        return index;
     }
 }
--- src/test/java/org/oenegm/MainTest.java
+++ src/test/java/org/oenegm/MainTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.oenegm.Main.bubbleSort;
+import static org.oenegm.Main.quickSort;
 
 class MainTest {
 
     @Test
-    void testSort() {
+    void testBubbleSort() {
         int[] arr = {23, 414, 4325, 123, 534, 1314, 532, 2423, 131, 67, 567, 456, 36745};
 
         int[] expected = {23, 67, 123, 131, 414, 456, 532, 534, 567, 1314, 2423, 4325, 36745};
@@ -18,4 +19,14 @@ class MainTest {
         assertArrayEquals(expected, arr);
     }
 
+    @Test
+    void testQuickSort() {
+        int[] arr = {23, 414, 4325, 123, 534, 1314, 532, 2423, 131, 67, 567, 456, 36745};
+
+        int[] expected = {23, 67, 123, 131, 414, 456, 532, 534, 567, 1314, 2423, 4325, 36745};
+
+        quickSort(arr);
+
+        assertArrayEquals(expected, arr);
+    }
 }
